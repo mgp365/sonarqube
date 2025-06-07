b9397a6f79dafdf29e1ea4e8de349a8c69cc5d8c
mgp365 <A00840918@tec.mx>
2025-06-07 17:41:41 -0600

    Update mul.h

--- mul.h
+++ mul.h
@@ -1,8 +1,18 @@
+
+
+/*
+ * Copyright (C) 2025 Tec de Monterrey
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include <math.h>
 #include <stdexcept>
 
 /**
-  metodo para realizar sumas de dos numeros
+  metodo para realizar multiplicación de dos numeros
  */
 double mul(int x, int y)
 {
